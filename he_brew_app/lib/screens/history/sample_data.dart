e8690e037b75ea46a72b7da349a2791879490c12
banaagcamille <20-06923@g.batstate-u.edu.ph>
2024-12-10 14:26:38 +0800

    testingdata

--- he_brew_app/lib/screens/history/sample_data.dart
+++ he_brew_app/lib/screens/history/sample_data.dart
@@ -1,8 +1,37 @@
 import 'history_item.dart';
+import 'package:flutter/material.dart';
 
 final List<HistoryItem> sampleHistory = [
-  HistoryItem(date: '2024-07-21', orderDetails: 'Latte', amount: 85.00),
-  HistoryItem(date: '2024-07-20', orderDetails: 'Cappuccino', amount: 85.00),
-  HistoryItem(date: '2024-07-19', orderDetails: 'Espresso', amount: 93.00),
-  HistoryItem(date: '2024-07-22', orderDetails: 'White Choco', amount: 83.00),
+  HistoryItem(
+    restaurant: 'Karam Beirut',
+    dateTime: '22 May 2024 - 10:00 PM',
+    orderId: '#20654834',
+    status: 'Successful',
+    statusColor: Colors.green,
+    amount: 85.00,
+  ),
+  HistoryItem(
+    restaurant: 'Karam Beirut',
+    dateTime: '21 May 2024 - 09:00 PM',
+    orderId: '#20654835',
+    status: 'Canceled',
+    statusColor: Colors.red,
+    amount: 90.00,
+  ),
+  HistoryItem(
+    restaurant: 'Karam Beirut',
+    dateTime: '20 May 2024 - 08:30 PM',
+    orderId: '#20654836',
+    status: 'Successful',
+    statusColor: Colors.green,
+    amount: 95.00,
+  ),
+  HistoryItem(
+    restaurant: 'Karam Beirut',
+    dateTime: '19 May 2024 - 07:45 PM',
+    orderId: '#20654837',
+    status: 'Successful',
+    statusColor: Colors.green,
+    amount: 92.00,
+  ),
 ];
